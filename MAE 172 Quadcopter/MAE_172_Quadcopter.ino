1575a1e43ff1931cd62e2e1b9d63f113bca42be8
ragnarok20 <stthayer@uci.edu>
2016-01-20 11:41:44 -0800

    Initial Commit

---
+++ MAE 172 Quadcopter/MAE_172_Quadcopter.ino
@@ -0,0 +1,68 @@
+//
+// MAE 172 Quadcopter
+//
+// Autonomous Quadcopter
+// Developed with [embedXcode](http://embedXcode.weebly.com)
+//
+// Author 		Sage Thayer
+// 				Sage Thayer
+//
+// Date			1/20/16 11:41 AM
+// Version		<#version#>
+//
+// Copyright	© Sage Thayer, 2016
+// Licence		<#licence#>
+//
+// See         ReadMe.txt for references
+//
+
+
+// Core library for code-sense - IDE-based
+#if defined(WIRING) // Wiring specific
+#   include "Wiring.h"
+#elif defined(MAPLE_IDE) // Maple specific
+#   include "WProgram.h"
+#elif defined(MPIDE) // chipKIT specific
+#   include "WProgram.h"
+#elif defined(DIGISPARK) // Digispark specific
+#   include "Arduino.h"
+#elif defined(ENERGIA) // LaunchPad specific
+#   include "Energia.h"
+#elif defined(LITTLEROBOTFRIENDS) // LittleRobotFriends specific
+#   include "LRF.h"
+#elif defined(MICRODUINO) // Microduino specific
+#   include "Arduino.h"
+#elif defined(SPARK) || defined(PARTICLE) // Particle / Spark specific
+#   include "Arduino.h"
+#elif defined(TEENSYDUINO) // Teensy specific
+#   include "Arduino.h"
+#elif defined(REDBEARLAB) // RedBearLab specific
+#   include "Arduino.h"
+#elif defined(ESP8266) // ESP8266 specific
+#   include "Arduino.h"
+#elif defined(ARDUINO) // Arduino 1.0 and 1.5 specific
+#   include "Arduino.h"
+#else // error
+#   error Platform not defined
+#endif // end IDE
+
+// Include application, user and local libraries
+
+
+// Prototypes
+
+
+// Define variables and constants
+
+
+// Add setup code
+void setup()
+{
+    ;
+}
+
+// Add loop code
+void loop()
+{
+    ;
+}
---
+++ MAE 172 Quadcopter/ReadMe.txt
@@ -0,0 +1,26 @@
+
+  MAE 172 Quadcopter
+  Project
+  ----------------------------------
+  Developed with embedXcode
+
+  Project MAE 172 Quadcopter
+  Created by Sage Thayer on 1/20/16
+  Copyright © 2016 Sage Thayer
+  Licence <#licence#>
+
+
+
+  References
+  ----------------------------------
+
+
+
+  embedXcode
+  embedXcode+
+  ----------------------------------
+  Embedded Computing on Xcode
+  Copyright © Rei VILO, 2010-2015
+  All rights reserved
+  http://embedXcode.weebly.com
+
